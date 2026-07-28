707198ee3becb122049694e1eaa4a15ff443c9c6
quiddam <quiddam@gmail.com>
2026-07-28 23:13:32 +0200

    Fix color scheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,15 +1,18 @@
+-- function ColorMyPencils(color)
+--   color = color or "rose-pine"
+--   vim.cmd.colorscheme(color)
 
-function ColorMyPencils(color)
-  color = color or "rose-pine"
-  vim.cmd.colorscheme(color)
+--  vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+--  vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+--  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--  vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
+--  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
-  vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
-  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+--end
 
-end
+-- ColorMyPencils()
+require("rose-pine").setup({
+    variant = "main",
+})
 
-ColorMyPencils()
-
+vim.cmd.colorscheme("rose-pine")
