285fc3aab1296bd54d1cb2013475a995342d0b60
quiddam <quiddam@gmail.com>
2026-09-02 15:54:10 +0200

    Add native vscode handling of git

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,16 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", function()
+    if vim.g.vscode then
+        local ok, vscode = pcall(require, "vscode")
+
+        if ok then
+            vscode.action("workbench.view.scm")
+        end
+
+        return
+    end
+
+    vim.cmd.Git()
+end, {
+    silent = true,
+    desc = "Open Git status",
+})
