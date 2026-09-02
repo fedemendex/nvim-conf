a99d6f37be5cbcc3256cf8f3dc5dcac9b1849622
quiddam <quiddam@gmail.com>
2026-09-02 15:33:52 +0200

    Highlight yanked text

--- init.lua
+++ init.lua
@@ -6,3 +6,31 @@ vim.g.loaded_netrwPlugin = 1
 require("luasnip.loaders.from_lua").lazy_load({
     paths = vim.fn.stdpath("config") .. "/lua/quiddam/snippets",
 })
+
+
+vim.api.nvim_set_hl(0, "YankHighlight", {
+    bg = "#7aa2f7",
+    fg = "#1a1b26",
+})
+
+local highlight_yank = vim.api.nvim_create_augroup(
+    "highlight-yank",
+    { clear = true }
+)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = highlight_yank,
+    callback = function()
+        if vim.v.event.operator ~= "y" then
+            return
+        end
+
+        local on_yank = vim.hl and vim.hl.on_yank
+            or vim.highlight.on_yank
+
+        on_yank({
+            higroup = "YankHighlight",
+            timeout = 100,
+        })
+    end,
+})
--- lua/quiddam/init.lua
+++ lua/quiddam/init.lua
@@ -5,3 +5,24 @@ require("quiddam.session")
 require("quiddam.remap")
 require("quiddam.packer")
 require("quiddam.set")
+
+local highlight_yank = vim.api.nvim_create_augroup(
+    "highlight-yank",
+    { clear = true }
+)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = highlight_yank,
+    desc = "Briefly highlight yanked text",
+    callback = function()
+        if vim.v.event.operator == "y" then
+            local on_yank = vim.hl and vim.hl.on_yank
+                or vim.highlight.on_yank
+
+            on_yank({
+                higroup = "Visual",
+                timeout = 250,
+            })
+        end
+    end,
+})
